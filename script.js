3db5e820ec15eedf06678398f255612b3638b3cd
emmad9146 <43858656+emmad9146@users.noreply.github.com>
2019-10-07 09:44:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Emma Doherty"
+let userName = "emmad9146";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
